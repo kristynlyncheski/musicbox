8bbcb1bbb6be518cdf0c0ed5feecbeb6c0891e6e
kristynlyncheski <klyncheski@gmail.com>
2016-05-06 11:32:04 -0400

    fix scrollbars

--- frontend/app/components/Songs.js
+++ frontend/app/components/Songs.js
@@ -10,7 +10,6 @@ const DropdownComp = React.createClass({
         <MenuItem key={index} eventKey={[playlist.playlist_id,this.props.songId,playlist.playlist_name]}>{playlist.playlist_name}</MenuItem>
       )
     });
-
     return(
       <DropdownButton
         pullRight
@@ -20,41 +19,9 @@ const DropdownComp = React.createClass({
         id={`dropdown-basic-${this.props.index}`}
         onSelect={this.props.onSelect}
       >
-        <ScrollArea style={{ maxHeight: 200, display: 'block', padding: '3px 20px', lineHeight: 1.42857143, color: '#333', whiteSpace: 'nowrap' }}
-        >
-          {playlists}
-        </ScrollArea>
+        {playlists}
       </DropdownButton>
     )
-    // if (this.props.songs.length > 4 || this.props.index < this.props.songs.length - 3)
-    // {
-    //   return(
-    //     <DropdownButton
-    //       pullRight
-    //       noCaret
-    //       dropup
-    //       title={<Icon>arrow_drop_down</Icon>}
-    //       key={this.props.index}
-    //       id={`dropdown-basic-${this.props.index}`}
-    //       onSelect={this.props.onSelect}
-    //     >
-    //       {playlists}
-    //     </DropdownButton>
-    //   )
-    // } else {
-    //   return(
-    //     <DropdownButton
-    //       pullRight
-    //       noCaret
-    //       title={<Icon>arrow_drop_down</Icon>}
-    //       key={this.props.index}
-    //       id={`dropdown-basic-${this.props.index}`}
-    //       onSelect={this.props.onSelect}
-    //     >
-    //       {playlists}
-    //     </DropdownButton>
-    //   )
-    // };
   },
 });
 
@@ -62,15 +29,6 @@ const Songs = React.createClass({
   render: function(){
     // console.log("saved",this.props.savedSongs);
     let savedSongs = this.props.savedSongs.map((song,index) => {
-      // let status = function(){
-      //   if (this.props.savedSongs.length < 3) {
-      //     return false
-      //   } else if (index <= this.props.savedSongs.length - 2) {
-      //     return true
-      //   } else {
-      //     return false
-      //   };
-      // };
       return (
         <div className="songs-collection" key={index}>
           <div className="saved-details">
@@ -80,7 +38,6 @@ const Songs = React.createClass({
           <div className="playlist-dropdown">
             <DropdownComp
               playlists={this.props.playlists}
-              songs={this.props.savedSongs}
               songId={song.song_id}
               index={index}
               onSelect={this.props.handlePlaylistAdd}
